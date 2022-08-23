78183ed86e69bd1d771f74ae2ce7fd4baada693e
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2022-08-22 21:07:08 -0300

    subi até o vendor kk

---
+++ .phan/config.php
@@ -0,0 +1,32 @@
+<?php
+//ferramenta para detectar erros no código
+
+return [
+
+    "target_php_version" => '8.0',
+
+    'directory_list' => [
+        'src',
+        'vendor/symfony/dom-crawler',
+        'vendor/guzzlehttp/guzzle',
+        'vendor/psr/http-message'
+    ],
+
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+
+    'plugins' => [
+        'AlwaysReturnPlugin',
+        'DollarDollarPlugin',
+        'DuplicateArrayKeyPlugin',
+        'DuplicateExpressionPlugin',
+        'PregRegexCheckerPlugin',
+        'PrintfCheckerPlugin',
+        'SleepCheckerPlugin',
+        'UnreachableCodePlugin',
+        'UseReturnValuePlugin',
+        'EmptyStatementListPlugin',
+        'LoopVariableReusePlugin',
+    ],
+];
