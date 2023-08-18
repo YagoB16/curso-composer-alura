28b29f4644e27b96938cfb9f77d8e96f5bb900a3
yago-bg <yago.gouveia@grupopll.com>
2023-08-18 13:59:56 -0300

    Commit

--- buscar-curso.php
+++ buscar-curso.php
@@ -1,15 +1,12 @@
-#!/usr/bin/ env php
 <?php
-
 require 'vendor/autoload.php';
-
 
 
 use GuzzleHttp\Client;
 use Alura\BuscadorCursos\Buscador;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['base_uri' => 'https://www.alura.com.br']);
+$client = new Client(['base_uri' => 'https://www.alura.com.br', 'verify' =>false]);
 $crawler = new Crawler();
 
 $buscador = new Buscador($client, $crawler);
